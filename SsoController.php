5f9882692d3ba69de6d3bb5bde58a327b9c8ce8f
thinfell · 尹 <thinfell@qq.com>
2016-09-22 16:02:28 +0800

    新增sso注册

--- SsoController.php
+++ SsoController.php
@@ -18,11 +18,28 @@ class SsoController extends Controller
     {
         $timestamp = microtime(true) * 10000;
         $sign = md5(Yii::$app->params['sso_website_id'].Yii::$app->params['sso_website_secret'].$timestamp);
-        $result = Curl::to(Yii::$app->params['sso_website_server'])->withData(['sso_website_id' => Yii::$app->params['sso_website_id'], 'sign' => $sign, 'timestamp' => $timestamp, 'AuthenTickitRequestParamName' => $AuthenTickitRequestParamName])->get();
+        $result = Curl::to(Yii::$app->params['sso_website_server'].'/login')->withData(['sso_website_id' => Yii::$app->params['sso_website_id'], 'sign' => $sign, 'timestamp' => $timestamp, 'AuthenTickitRequestParamName' => $AuthenTickitRequestParamName])->get();
         $result = json_decode($result);
         if($result->code == 200){
-            $userid = $result->message;// 返回唯一用户 userid 
-            return Yii::$app->user->login(User::findByUserid($userid), 0);// 修改为自己站点的登录方法 
+            $account = $result->message;
+            return Yii::$app->user->login(User::findByUsername($account), 0);
+        }else{
+            Yii::$app->response->data =['code' => 0, 'message' => $result->message];
+            return false;
+        }
+    }
+
+    public function actionRegister($AuthenTickitRequestParamName)
+    {
+        $timestamp = microtime(true) * 10000;
+        $sign = md5(Yii::$app->params['sso_website_id'].Yii::$app->params['sso_website_secret'].$timestamp);
+        $result = Curl::to(Yii::$app->params['sso_website_server'].'/register')->withData(['sso_website_id' => Yii::$app->params['sso_website_id'], 'sign' => $sign, 'timestamp' => $timestamp, 'AuthenTickitRequestParamName' => $AuthenTickitRequestParamName])->get();
+        $result = json_decode($result);
+        if($result->code == 200){
+            $user = $result->message;
+            //注册$user
+            //登录$user
+            return true;
         }else{
             Yii::$app->response->data =['code' => 0, 'message' => $result->message];
             return false;
